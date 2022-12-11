153d995ed082551f3b69c86bca1b98a3a3836270
Steve Kim <stevekim4@kakao.com>
2022-12-11 14:19:45 +0900

    #3.4 Code Actions -- DONE

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,72 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Hello Flutter!'),
-        ),
-        body: const Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+          backgroundColor: const Color(0xFF181818),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 80,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        const Text("Hey, RaonZena",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 28,
+                                fontWeight: FontWeight.w800)),
+                        Text("Welcome Back",
+                            style: TextStyle(
+                              color: Colors.white.withOpacity(0.5),
+                              fontSize: 18,
+                            )),
+                      ],
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 120,
+                ),
+                Text('Total Balance',
+                    style: TextStyle(
+                      color: Colors.white.withOpacity(0.5),
+                      fontSize: 22,
+                    )),
+                const SizedBox(height: 5),
+                const Text('\$ 5 194 482',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 45,
+                      fontWeight: FontWeight.w600,
+                    )),
+                const SizedBox(height: 30),
+                Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(45),
+                      ),
+                      child: const Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+                        child: Text('Transfer',
+                            style: TextStyle(
+                              fontSize: 20,
+                            )),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
